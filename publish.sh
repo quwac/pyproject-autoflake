5da5dcb0be71418f40e839834c8b11ce404ff650
quwac <53551867+quwac@users.noreply.github.com>
2022-01-09 23:10:45 +0900

    Modify publish script

--- publish.sh
+++ publish.sh
@@ -2,16 +2,26 @@
 
 set -eu
 
-if [ ! -f "./pypi.env" ]; then
-    echo "./pypi.env not found."
+# shellcheck disable=SC2046,SC2086
+PROJECT_PATH="$(cd $(dirname $0) && pwd)"
+PYPI_ENV_PATH="$PROJECT_PATH/pypi.env"
+DIST_PATH="$PROJECT_PATH/dist"
+
+if [ ! -f "$PYPI_ENV_PATH" ]; then
+    echo "$PYPI_ENV_PATH not found."
     exit 1
 fi
 
-# shellcheck disable=SC1091
-source "./pypi.env"
+if [ -e "$DIST_PATH" ]; then
+    rm -rf "$DIST_PATH"
+fi
+
+# shellcheck disable=SC1090,SC1091
+source "$PYPI_ENV_PATH"
 
 # shellcheck disable=SC2154
-poetry publish --build \
+poetry publish \
     -r "$repository" \
     -u "$username" \
-    -p "$password"
+    -p "$password" \
+    --build
